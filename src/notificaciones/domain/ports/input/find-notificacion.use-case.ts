3f53ac7f5d80148ae576f96a06211a3e2a833005
Jhan Breyner Manrique <manriquejhanbreyner@gmail.com>
2026-07-09 22:36:37 -0500

    refactor: modularizar funciones repetidas

--- src/notificaciones/domain/ports/input/find-notificacion.use-case.ts
+++
@@ -1,7 +0,0 @@
-import { Notificacion } from '../../entities/notificacion.entity';
-import { PaginatedResult } from '../output/notificacion.repository.port';
-
-export interface FindNotificacionUseCase {
-    findAll(page: number, limit: number): Promise<PaginatedResult<Notificacion>>;
-    findById(id: string): Promise<Notificacion>;
-}
--- src/notificaciones/domain/ports/output/notificacion.repository.port.ts
+++
@@ -1,9 +0,0 @@
-import { Notificacion } from '../../entities/notificacion.entity';
-
-export interface PaginatedResult<T> { data: T[]; total: number; page: number; limit: number; }
-
-export interface NotificacionRepositoryPort {
-    findAll(page: number, limit: number): Promise<PaginatedResult<Notificacion>>;
-    findById(id: string): Promise<Notificacion | null>;
-    save(entity: Notificacion): Promise<Notificacion>;
-}
