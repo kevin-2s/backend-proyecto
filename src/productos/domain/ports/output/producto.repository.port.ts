6560098156fecc46b607f268bf734ce036e51ba5
Jhan Breyner Manrique <manriquejhanbreyner@gmail.com>
2026-07-09 22:36:40 -0500

    feat: implementar modulo mejorado

--- src/productos/domain/ports/output/producto.repository.port.ts
+++
@@ -1,9 +0,0 @@
-import { Producto } from '../../entities/producto.entity';
-
-export interface PaginatedResult<T> { data: T[]; total: number; page: number; limit: number; }
-
-export interface ProductoRepositoryPort {
-    findAll(page: number, limit: number): Promise<PaginatedResult<Producto>>;
-    findById(id: string): Promise<Producto | null>;
-    save(entity: Producto): Promise<Producto>;
-}
